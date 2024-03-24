c6f21e639585b17be7d14ff77237031cec16d07b
oliver-rafael <rffael@gmail.com>
2024-03-24 23:45:46 +0000

    code & graficos

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ebac-mod16
-Análise Exploratória de Dados
+# CONTEXTO
+
+Esse projeto faz parte da conclusão do módulo 16 do curso de Análise de Dados da EBAC. A proposta foi de escolher um data set na plataforma Kaggle com a finalidade de realizar uma Análise Exploratória de Dados (AED).
+
+Atualmente o Brasil vive uma crise epidemiológica de dengue, assunto escolhido para realizar a AED. Como base de dados foi utilizado o data set com os dados dos casos confirmados de dengue, na cidade de Campinas / SP. Levantamento feito entre os anos de 1998 e 2014, constando o número de casos confirmados no município além de dados de temperatura e o volume de chuva ao longo desses anos.
+
+# INSIGHTS
+
+Após as etapas de manipulação e exploração dos dados, foram gerados alguns gráficos com o objetivo de entender melhor o comportamento do aumento no número de casos e levantar alguns insights que talvez possam orientar para um controle da crise que vivemos hoje.
+
+Observando o gráfico (graf_01) com número total de casos confirmados agrupados por cada ano, é possível observar que entre os anos de 1998 a 2006 a doença permaneceu controlada com o número de casos abaixo de 1.000 e na maioria dos anos nesse período abaixo de 100 casos. Apenas em 2007 os casos ultrapassam um pouco mais da faixa dos 10.000 casos. Caindo novamente nos anos seguintes próximos aos índices anteriores, mas subindo exponencialmente no ano de 2014, quando o município atinge o índice de epidemia.
+
+Segundo a OMS, taxas acima de 300 casos por 100 mil habitantes indicam situação epidêmica. Em 2014, a população de Campinas-SP segundo o IBGE era de 1.154.617 habitantes, portanto a taxa epidêmica nesse ano foi de 3646/100mil.
+
+A partir do gráfico (graf_02), quando agrupados os dados em total de casos por meses, observa-se que os primeiros meses do ano são os que concentram o maior número de casos, isso se dá pela ocorrência de chuvas nesse período e média elevada da temperatura, o que proporciona um ambiente favorável à proliferação do mosquito transmissor da dengue. De acordo com o cenário, o período mais crítico é de março a abril.
+
+Na comparação feita entre os anos 2004 e 2014, menor e maior número de casos respectivamente, (graf_03 e graf_04), observa-se que apesar da grande diferença na quantidade de casos, ainda sim se mantém o padrão de concentração das ocorrências nos primeiros 4 meses do ano. Por fim, observou-se com os dados de temperatura e volume médio de chuva, o comportamento do clima, também nesses dois anos (graf_05 e graf_06). Verifica-se através dos gráficos que em 2004 a média de chuva foi maior com relação a 2014, ao longo dos 4 primeiros meses do ano, em contrapartida a média de temperatura em 2004 para o mesmo período é mais baixa, quando comparados os anos, o que talvez seja um fator favorável ao mosquito e tenha contribuído no grande número de casos, relação que precisa ser melhor analisada quando cruzada com dados de estudos sobre o mosquito transmissor da dengue, entre outros fatores climáticos e até mesmo sociais.
+
+Observando a relação das médias de chuva e temperatura com a quantidade de casos, verifica-se que o maior número de casos concentra-se entre os quatro primeiros meses do ano, quando as médias de temperatura mantêm-se mais elevadas. Ainda sobre os fatores temperatura e chuva, talvez possamos dizer que dentre eles, o mosquito da dengue seja mais sensível a mudanças da temperatura, visto que comparados os anos de 2004 e 2014 o volume de chuva é muito parecido enquanto na temperatura, no ano de 2004 a temperatura média dos primeiros meses manteve-se abaixo dos 24°C.
+
+Analisando os dados conclui-se que houve uma crescente no número de casos ao longo do tempo, fatores como o aumento da população, a ocupação de áreas preservadas, falta de saneamento, que são problemas comuns nos grandes centros urbanos, de certa forma contribuem para um ambiente de proliferação do mosquito transmissor da dengue e potencializam as variantes climáticas que também tem influência na reprodução do mosquito, como o aquecimento global, o regime de chuvas, etc. É um tema multidisciplinar, e para que possamos entender melhor como gerenciar e controlar essa doença, requer que se aprofunde as pesquisas, cruzando informações de forma ampla e analítica.
+
--- aed_dengue.py
+++ aed_dengue.py
@@ -7,4 +7,123 @@ pd.options.plotting.backend = "plotly"
 
 # - coleta de dados; > Data Set da plaataforma kaggle disponivel em:  https://www.kaggle.com/datasets/renangomes/dengue-temperatura-e-chuvas-em-campinassp
 df_dengue = pd.read_csv('./cases_dengue.csv', delimiter = ',')
-df_dengue.tail()
+
+# separando a data entre ano e mes
+df_dengue['data'] = pd.to_datetime(df_dengue['data'])
+df_dengue['ano'] = df_dengue['data'].dt.year
+df_dengue['mes'] = df_dengue['data'].dt.month_name()
+df_dengue ['ano'] = pd.to_numeric(df_dengue['ano'])
+
+#renomeando e reordenando colunas
+df_dengue.rename(columns={'casos-confirmados':'casos_confirmados','chuva':'volume_chuva', 'temperatura-media': 'temperatura_media','temperatura-mininima': 'temperatura_minima', 'temperatura-maxima': 'temperatura_maxima'}, inplace= True)
+df_dengue = df_dengue[['ano', 'mes', 'casos_confirmados', 'volume_chuva','temperatura_media','temperatura_minima', 'temperatura_maxima']]
+
+# média de volume de chuva por ano e mês
+#transform() permite aplicar a funcao media a cada valor do agrupamento
+media_por_ano_mes = df_dengue.groupby('mes')['volume_chuva'].transform('mean')
+
+# substituir os valores nulos pela média por ano e mês
+df_dengue['volume_chuva'] = df_dengue['volume_chuva'].fillna(media_por_ano_mes).round()
+
+##Visualizaçoes 
+
+sns.set(rc={'figure.figsize':(12,8)}) # dimensões para o grafico
+
+#  total de caso em anos
+casos_anos = df_dengue[['ano', 'casos_confirmados']].groupby('ano').agg('sum')
+
+# Grafico 01 - visualizacao do total a cada ano da serie
+with sns.axes_style('darkgrid'):
+  graf_01 = sns.barplot(data = casos_anos, x ='ano', y ='casos_confirmados', ci = None, palette = 'dark')
+  graf_01.set(title = "Casos Confirmados de Dengue em Campinas - SP (1998 - 2014)", xlabel= 'Ano', ylabel = 'Quantidade')
+  plt.xticks(rotation=45)
+  #adiciona valores a cada coluna
+  for i in graf_01.patches:
+    graf_01.annotate(i.get_height(),
+                            (i.get_x()+ i.get_width() / 2, i.get_height()),
+                            ha='center', va = 'baseline', fontsize = 8,
+                            color= 'black', xytext=(0,1),
+                            textcoords = 'offset pixels')
+
+  graf_01.fifure.savefig('graf_01')
+
+# grafico 02 visualizacao do total a cada mes da serie
+df_months= df_dengue[['mes','casos_confirmados']].groupby('mes').agg('sum')
+df_months = df_months.sort_values(by='casos_confirmados' )
+with sns.axes_style('darkgrid'):
+  graf_02= sns.barplot(data = df_months, x ='mes', y ='casos_confirmados', ci = None, palette = 'dark')
+  graf_02.set(title = "Total de Casos de Dengue Durante os Meses em Campinas - SP (1998 - 2014)", xlabel= 'Ano', ylabel = 'Número de Casos')
+  plt.xticks(rotation=45)
+
+  #adiciona valores a cada coluna
+  for i in graf_02.patches:
+    graf_02.annotate(i.get_height(),
+                            (i.get_x()+ i.get_width() / 2, i.get_height()),
+                            ha='center', va = 'baseline', fontsize = 8,
+                            color= 'black', xytext=(0,1),
+                            textcoords = 'offset pixels')
+  
+  graf_02.figure.savefig('graf_02')
+
+# filtro dos anos extremos menor(2004) e maior quantidade de casos(2014)
+df_years = df_dengue[df_dengue['ano'].isin([2004, 2014])]
+df_y04 = df_dengue[df_dengue['ano'].isin([2004])]
+df_y14 = df_dengue[df_dengue['ano'].isin([2014])]
+
+#grafico 03 total de casos em 2004
+graf_03 = plt.figure(figsize = ( 12, 8 ))
+plt.scatter( x = 'mes' , y = 'casos_confirmados', data = df_y04)
+plt.xlabel( 'Meses' , size = 12)
+plt.xticks(rotation=45)
+plt.ylabel( 'Quantidade de casos' , size = 12 )
+plt.title( 'Casos de Dengue nos Meses de 2004 em Campinas - SP' , size = 14 )
+
+graf_03.figure.savefig('graf_03')
+
+#grafico 04 total de casos em 2014
+graf_04 = plt.figure(figsize = ( 12, 8 ))
+plt.scatter( x = 'mes' , y = 'casos_confirmados', data = df_y14)
+plt.xlabel( 'Meses' , size = 12)
+plt.xticks(rotation=45)
+plt.ylabel( 'Quantidade de casos' , size = 12 )
+plt.title( 'Casos de Dengue nos Meses de 2014 em Campinas - SP' , size = 14 )
+
+graf_04.figure.savefig('graf_04')
+
+#df com os dois anos agrupados em 
+df_04_14 = df_years.groupby(['ano', 'mes'])['casos_confirmados'].sum().reset_index().pivot('mes', 'ano', 'casos_confirmados')
+
+#juntando os dois graficos anteriores em um gráfico  interativo
+fig = df_04_14.plot(kind='barh', title="Casos confirmados de dengue em Campinas para os anos de 2004 e 2014")
+# Define o nome dos eixos
+fig.update_layout(xaxis_title='Número de casos confirmados', yaxis_title='Mês')
+#centraliza o titulo
+fig.update_layout(title_x=0.5)
+
+# variacao da chuva nos dois anos 04/14
+with sns.axes_style('darkgrid'):
+  graf_05 = sns.lineplot(data=df_years, x='mes', y='volume_chuva', hue='ano', palette='dark')
+  plt.xticks(rotation=45)
+  graf_05.set(title='Média de Chuva  Em Campinas - SP - 2004 e 2014', xlabel='Meses', ylabel='Volume em (mm)');
+  graf_05.get_legend().set_title("Ano");
+  graf_05.figure.savefig('graf_05')
+
+#variacao datemperatura nos dois anos 04/14
+with sns.axes_style('darkgrid'):
+
+  graf_06 = sns.lineplot(data=df_years, x='mes', y='temperatura_media', hue='ano', palette='dark')
+  graf_06.set(title='Média Temperatura Em campinas-SP - 2004 e 2014', xlabel='Meses', ylabel='Temperatura (°C)')
+  plt.xticks(rotation=45)
+  graf_06.get_legend().set_title("Ano");
+  graf_06.figure.savefig('graf_06')
+
+
+
+
+
+
+
+
+
+
+
